0a580e957e5bb6774661ef6c30fc9616a2ccc682
jarltuxen <jarl.tuxen@gmail.com>
2021-11-05 15:48:45 +0100

    Initial commit

---
+++ src/test/java/SeleniumTest.java
@@ -0,0 +1,33 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.openqa.selenium.By;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+
+public class SeleniumTest {
+
+    @Test
+    public void fireFoxTest(){
+        //set headless with options in FirefoxDriver constructor
+        FirefoxOptions options = new FirefoxOptions();
+        options.setHeadless(true);
+        WebDriver driver = new FirefoxDriver(options);
+        //get webpage
+        driver.get("http://demo.guru99.com/");
+        //find email input field and enter mail address
+        WebElement element = driver.findElement(By.name("emailid"));
+        element.sendKeys("abc@gmail.com");
+        //find login button and click
+        WebElement button = driver.findElement(By.name("btnLogin"));
+        button.click();
+        //clean up browserdriver - if visible close tab used
+        System.out.println("Title of page is: " + driver.getTitle());
+        assertTrue(driver.getTitle().equals("Guru99 Bank Home Page"));
+        //driver.wait(3000);
+        driver.close();
+    }
+}
